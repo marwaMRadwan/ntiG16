125c845c7211f0ec577f1cb00be233ba67eb2fff
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-06 11:41:54 +0200

    s1

--- session1/custom.js
+++ session1/custom.js
@@ -50,28 +50,91 @@ check conversion type
 convert to c
 convert to f
 */
-const getUserInput = (msg)=>{
-    return prompt(msg)
-}
-const convertToC = (val)=>{
-    return (Number(val)-32)*5/9
-}
-const convertToF = (val)=>{
-    return (parseFloat(val)*9/5)+32
-}
+// const getUserInput = (msg)=>{
+//     return prompt(msg)
+// }
+// const convertToC = (val)=>{
+//     return (Number(val)-32)*5/9
+// }
+// const convertToF = (val)=>{
+//     return (parseFloat(val)*9/5)+32
+// }
+// const myConvert = () => {
+//     const convertType = getUserInput("your convert type (choose between c and f)")
+//     const temp = getUserInput("temp: ")
+//     let result = ""
+//     switch(convertType){
+//         case 'c':result= convertToC(temp);break;
+//         case 'f':result= convertToF(temp);break;
+//         default: result="undefined type";
+//     }
+//     return result
+// }
+//alert(myConvert())
 
-const myConvert = () => {
-    const convertType = getUserInput("your convert type (choose between c and f)")
-    const temp = getUserInput("temp: ")
-    let result = ""
-    switch(convertType){
-        case 'c':result= convertToC(temp);break;
-        case 'f':result= convertToF(temp);break;
-        default: result="undefined type";
-    }
-    return result
+// let x = [1, false, "hello", 5.5]
+// for(a of x) console.log(a)
+// x.length
+// for(i=0;i<x.length;i++){
+//     console.log(x[i])
+// }
+// x.forEach((el, i)=>{
+//     console.log(i, "=>", el);
+// })
+// find findIndex filter
+
+//object
+// let user = {
+//     name:"marwa",
+//     age:36,
+//     job:"developer"
+// }
+// for(o in user){
+//     console.log(o , "=", user[o])
+// }
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+
+// let users = [
+//     {
+//         name:"marwa",
+//         age:36,
+//         job:"developer"
+//     },
+//     {
+//         name:"mohammed",
+//         age:40,
+//         job:"doctor"
+//     }   
+// ]
+// for(o in x){
+//     console.log(o , "=", x[o])  
+// }
+// users[0]
+// console.log(user.name)
+// console.log(user['name'])
+// object
+/*
+{
+    0: x,
+    1:y,
+    2:i
 }
-alert(myConvert())
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
