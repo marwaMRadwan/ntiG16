11df5a72484c968afdebc411544aaae09977bf27
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-09 10:43:47 +0200

    s4

---
+++ session4/fNode/app.js
@@ -0,0 +1,13 @@
+// console.log("hello world")
+
+//modules
+//build-in modules
+//fs => file system
+// const myFs = require("fs")
+// myFs.writeFileSync("a.txt", "hello")
+
+//my own modules
+const myMod = require("./m")
+// myMod()
+// console.log(myMod)
+myMod.fun2()
---
+++ session4/fNode/m.js
@@ -0,0 +1,7 @@
+const fun1 = () =>{
+    console.log("fun1")
+}
+const fun2 = () =>{
+    console.log('fun2')
+}
+module.exports = { fun1 , fun2 } // {fun1:fun1, fun2:fun2}
