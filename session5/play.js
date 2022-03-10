77e3d182b7c9102d8304bd3e610aa2c5250f3567
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-10 10:00:00 +0200

    s5

--- session5/play.js
+++ session5/play.js
@@ -1,11 +1,13 @@
-const users = [
-    {id:1, name:"marwa", age:36},
-    {id:2, name:"xy", age:37},
-    {id:3, name:"ab", age:38},
-    {id:4, name:"dc", age:39}
-]
-const fs = require("fs")
+// const users = [
+//     {id:1, name:"marwa", age:36},
+//     {id:2, name:"xy", age:37},
+//     {id:3, name:"ab", age:38},
+//     {id:4, name:"dc", age:39}
+// ]
+// const fs = require("fs")
 // fs.writeFileSync("data.json", JSON.stringify(users))
 
-const data = JSON.parse(fs.readFileSync("data.json"))
-console.log(data)
+// const data = JSON.parse(fs.readFileSync("data.json"))
+// console.log(data)
+const x = [1,2,3,4,5]
+console.log([9,...x, 6])
