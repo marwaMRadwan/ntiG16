c9b595cb522a7f82f65815861213ae7edb727f6a
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-13 12:09:52 +0200

    s6

--- session6/hbsEngine/app.js
+++ session6/hbsEngine/app.js
@@ -1,12 +1,15 @@
 const express = require("express")
+const path = require("path")
+const hbs = require('hbs')
 const app = express()
 const PORT = 3000
-//engine type hbs ejs pug
-//npm i hbs
+const viewDir = path.join(__dirname, "public/frontend/views")
+const layoutDir = path.join(__dirname, "public/frontend/layouts")
+//engine type hbs ejs pug  //npm i hbs
 app.set("view engine", "hbs")
-
+app.set('views', viewDir)
+hbs.registerPartials(layoutDir)
 app.get("/",(req,res)=>{
     res.render('home')
 })
-
 app.listen(PORT, ()=> console.log("server up"))
