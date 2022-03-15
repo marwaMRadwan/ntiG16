41ef79506f3b3033cb2f731830b66f70fd8ee3c1
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-15 11:08:56 +0200

    s7

--- session7/app/controllers/user.controller.js
+++ session7/app/controllers/user.controller.js
@@ -1,5 +1,25 @@
-const index = (req,res)=>{
-    res.render("home")
+const showAll = (req,res)=>{
+    res.render("showAll", {
+        pageTitle:"All Users"
+    })
+}
+const show = (req,res)=>{
+    res.render("show", {
+        pageTitle:"User Data"
+    })
 }
 
-module.exports = { index }
+const addUser = (req,res)=>{
+    res.render("add", {
+        pageTitle:"Add New User"
+    })
+}
+const editUser = (req,res)=>{
+    res.render("edit", {
+        pageTitle:"Edit User"
+    })
+}
+const deleteUser = (req,res)=>{
+    res.send("deleted")
+}
+module.exports = { showAll, addUser, editUser, show , deleteUser}
--- session7/routes/user.routes.js
+++ session7/routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router()
 const userController = require('../app/controllers/user.controller')
 
-router.get("/", userController.index)
-
+router.get("/", userController.showAll)
+router.get("/add", userController.addUser)
+router.get("/edit", userController.editUser)
+router.get("/show", userController.show)
+router.get("/delete", userController.deleteUser)
 module.exports = router
