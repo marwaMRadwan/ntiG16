9b08daaafa2dede6084c800099f7f53cdb5f357f
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-15 12:59:08 +0200

    s7

--- session7/app/controllers/user.controller.js
+++ session7/app/controllers/user.controller.js
@@ -1,4 +1,6 @@
+const deal = require("../helpers/dealWithJson")
 const showAll = (req,res)=>{
+    
     res.render("showAll", {
         pageTitle:"All Users"
     })
@@ -8,10 +10,21 @@ const show = (req,res)=>{
         pageTitle:"User Data"
     })
 }
-
 const addUser = (req,res)=>{
+    let user = {
+        name:req.query.name,
+        age:req.query.age,
+        email:req.query.email
+    }
+    if(req.query.name && req.query.email && req.query.age){
+        let data = deal.readData()
+        data.push(user)
+        deal.writeData(data)
+        res.redirect("/")
+    }
     res.render("add", {
-        pageTitle:"Add New User"
+        pageTitle:"Add New User",
+        user
     })
 }
 const editUser = (req,res)=>{
