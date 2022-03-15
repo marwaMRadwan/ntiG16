2c0a19028cd1fbb1134d017ca537365662b3691f
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-15 10:30:02 +0200

    s7

--- session7/routes/user.routes.js
+++ session7/routes/user.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router()
+const userController = require('../app/controllers/user.controller')
 
-router.get("/", (req,res)=>{
-    res.render("home")
-})
+router.get("/", userController.index)
 
 module.exports = router
