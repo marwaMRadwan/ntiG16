f8f774ae6ba45dd503fc741110f96adcece5c0c8
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-16 15:24:54 +0200

    s8

--- session8/monogoDbSession/index.js
+++ session8/monogoDbSession/index.js
@@ -6,11 +6,29 @@ const dbName = "s8"
 MongoClient.connect( dbUrl, {}, (err, client)=>{
     if(err) return console.log("database error")
     const connection = client.db(dbName)
-    connection.collection('user').insertOne(
-        {name:"nouran", age:28, email:"nouran@techsexperts.com"},
-        (error, res)=>{
-            if(error) return console.log("error insert data")
+    // connection.collection('user').insertOne(
+    //     {name:"nouran", age:28, email:"nouran@techsexperts.com"},
+    //     (error, res)=>{
+    //         if(error) return console.log("error insert data")
+    //         console.log(res)
+    //     }
+    // )
+    //     connection.collection('user').insertMany(
+    //     [
+    //         {name:"nouran", age:28, email:"nouran@techsexperts.com"},
+    //         {name:"omar", age:7}
+    //     ],
+    //     (error, res)=>{
+    //         if(error) return console.log("error insert data")
+    //         console.log(res)
+    //     }
+    // )
+    connection.collection("user").find({name:"marwa"}).toArray(
+        (err, res)=>{
+            if(err) return console.log("db error")
             console.log(res)
         }
     )
 })
+
+
