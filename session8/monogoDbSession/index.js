5c133b1d66ee6b02cbd9387cdd690b7c5ab580fb
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-16 13:05:34 +0200

    s8

---
+++ session8/monogoDbSession/index.js
@@ -0,0 +1,16 @@
+// const mongodb = require("mongodb")
+//const MongoClient = mongodb.MongoClient
+const { MongoClient } = require("mongodb")
+const dbUrl = "mongodb://127.0.0.1:27017"
+const dbName = "s8"
+MongoClient.connect( dbUrl, {}, (err, client)=>{
+    if(err) return console.log("database error")
+    const connection = client.db(dbName)
+    connection.collection('user').insertOne(
+        {name:"nouran", age:28, email:"nouran@techsexperts.com"},
+        (error, res)=>{
+            if(error) return console.log("error insert data")
+            console.log(res)
+        }
+    )
+})
