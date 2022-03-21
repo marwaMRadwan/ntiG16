05240b9da190c0df0b506484f110dd2904c11c1b
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-21 15:10:07 +0200

    s11

--- session10-11/routes/user.routes.js
+++ session10-11/routes/user.routes.js
@@ -1,10 +1,10 @@
 const userController = require("../controller/user.controller")
 const router = require("express").Router()
-
+const auth = require("../middleware/auth")
 router.post("/register", userController.add)
 router.post("/login", userController.login)
 
-router.get('/all', userController.all)
+router.get('/all',auth, userController.all)
 router.get('/all/:id', userController.single)
 router.delete('/all/:id', userController.del)
 
