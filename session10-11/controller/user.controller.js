2fe37434fdd30b1fbd54179d53381e6056818a34
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-21 11:54:23 +0200

    s11

--- session10-11/controller/user.controller.js
+++ session10-11/controller/user.controller.js
@@ -35,6 +35,43 @@ class User{
             })
         }
     }
+    static single = async(req,res)=>{
+        try{
+            const user = await userModel.findById(req.params.id)
+            // const user = await userModel.findOne({_id:req.params.id})
+            // const user = await userModel.find({_id:req.params.id})
+            res.status(200).send({
+                apiStatus:true,
+                data:user,
+                message:"users fetched"
+            })
+        }
+        catch(e){
+            res.status(500).send({
+                apiStatus:false,
+                errors:e.message,
+                message:"error in fetching"
+            })
+        }
+    }
+    static del = async(req,res)=>{
+        try{
+            const user = await userModel.findByIdAndDelete(req.params.id)
+            res.status(200).send({
+                apiStatus:true,
+                data:user,
+                message:"user deleted"
+            })
+        }
+        catch(e){
+            res.status(500).send({
+                apiStatus:false,
+                errors:e.message,
+                message:"error in deleting"
+            })
+        }
+
+    }
 }
 
 module.exports = User
--- session10-11/routes/user.routes.js
+++ session10-11/routes/user.routes.js
@@ -1,5 +1,10 @@
 const userController = require("../controller/user.controller")
 const router = require("express").Router()
+
 router.post("/register", userController.add)
 router.get('/all', userController.all)
+router.get('/all/:id', userController.single)
+router.delete('/all/:id', userController.del)
+
 module.exports = router
+
