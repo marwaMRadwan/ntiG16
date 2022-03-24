298dfec674aff57f51750b619b868f5a45d522e8
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-24 13:23:14 +0200

    s14

--- session14/src/app/app.component.ts
+++ session14/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Image} from 'src/app/interfaces/image'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,31 +7,104 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title :string= 'session14';
-  img:string="assets/img.jpg"
-  data = "<h3>hello</h3><p>hi</p>"
-  testEvent = ""
+  // title :string= 'session14';
+  // img:string="assets/img.jpg"
+  // data = "<h3>hello</h3><p>hi</p>"
+  // testEvent = ""
 
-  val : number = 0
+  // val : number = 0
   
-  myData(): string{
-    return "Hi There"
-  }
+  // myData(): string{
+  //   return "Hi There"
+  // }
 //click hover mouseup mousedown input ngSubmit
-  handleClick(){
-    this.testEvent = "hello"
-    console.log("hello")
-  }
+  // handleClick(){
+  //   this.testEvent = "hello"
+  //   console.log("hello")
+  // }
 
-  handleBtn(type:string){
-    if(type=="+") this.val++
-    else this.val--
-  }
+  // handleBtn(type:string){
+  //   if(type=="+") this.val++
+  //   else this.val--
+  // }
 
-  inputData=""
-  handleInput(event:any){
-    // console.log(event.target.value)
-    this.inputData = event.target.value
-  }
+  // inputData=""
+  // handleInput(event:any){
+  //   // console.log(event.target.value)
+  //   this.inputData = event.target.value
+  // }
 
+  // myData:string = "marwa"
+  photos:Image[] = [
+    {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+    },
+    {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796"
+    },
+    {
+    albumId: 1,
+    id: 3,
+    title: "officia porro iure quia iusto qui ipsa ut modi",
+    url: "https://via.placeholder.com/600/24f355",
+    thumbnailUrl: "https://via.placeholder.com/150/24f355"
+    },
+    {
+    albumId: 1,
+    id: 4,
+    title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    url: "https://via.placeholder.com/600/d32776",
+    thumbnailUrl: "https://via.placeholder.com/150/d32776"
+    },
+    {
+    albumId: 1,
+    id: 5,
+    title: "natus nisi omnis corporis facere molestiae rerum in",
+    url: "https://via.placeholder.com/600/f66b97",
+    thumbnailUrl: "https://via.placeholder.com/150/f66b97"
+    },
+    {
+    albumId: 1,
+    id: 6,
+    title: "accusamus ea aliquid et amet sequi nemo",
+    url: "https://via.placeholder.com/600/56a8c2",
+    thumbnailUrl: "https://via.placeholder.com/150/56a8c2"
+    },
+    {
+    albumId: 1,
+    id: 7,
+    title: "officia delectus consequatur vero aut veniam explicabo molestias",
+    url: "https://via.placeholder.com/600/b0f7cc",
+    thumbnailUrl: "https://via.placeholder.com/150/b0f7cc"
+    },
+    {
+    albumId: 1,
+    id: 8,
+    title: "aut porro officiis laborum odit ea laudantium corporis",
+    url: "https://via.placeholder.com/600/54176f",
+    thumbnailUrl: "https://via.placeholder.com/150/54176f"
+    },
+    {
+    albumId: 1,
+    id: 9,
+    title: "qui eius qui autem sed",
+    url: "https://via.placeholder.com/600/51aa97",
+    thumbnailUrl: "https://via.placeholder.com/150/51aa97"
+    },
+    {
+    albumId: 1,
+    id: 10,
+    title: "beatae et provident et ut vel",
+    url: "https://via.placeholder.com/600/810b14",
+    thumbnailUrl: "https://via.placeholder.com/150/810b14"
+    }
+    ]
 }
