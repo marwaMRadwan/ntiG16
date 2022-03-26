97c2e1376795fb7cd9879fa8a5c29ed8c00e808d
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-26 13:54:53 +0200

    s15

--- session15/src/app/pages/contact/contact.component.ts
+++ session15/src/app/pages/contact/contact.component.ts
@@ -6,14 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  month : any
-  months: string[] = ['jan','feb','mar','apr','may','jun', 'jul', 'aug', 'sep', 'oct','nov','dec']
-  s:string[]=['rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'ram', 'monkey', 'rooster','dog', 'pig']
+  // month : any
+  // months: string[] = ['jan','feb','mar','apr','may','jun', 'jul', 'aug', 'sep', 'oct','nov','dec']
+  // s:string[]=['rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'ram', 'monkey', 'rooster','dog', 'pig']
+  d :Date = new Date()
+  name="MaRwA"
+  x:number= 5.125844521515
+  user = {name:"marwa", age:36, job:"developer"}
   constructor() { }
 
   ngOnInit(): void {
   }
-  changeVal(event:any){
-    this.month = event.target.value
-  }
+  // changeVal(event:any){
+  //   this.month = event.target.value
+  // }
 }
