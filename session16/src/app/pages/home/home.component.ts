7e448800d8be23d1be1f0929ff166a43fb81c509
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-27 11:43:25 +0200

    s16

--- session16/src/app/pages/home/home.component.ts
+++ session16/src/app/pages/home/home.component.ts
@@ -7,13 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  msg : string = ""
+  resFlag:boolean = false
   constructor() { }
 
   ngOnInit(): void {
   }
   handleForm(myForm:NgForm){
-    console.log(myForm.value)
+    if(myForm.valid){
+      console.log(myForm.value)
+      this.msg="data added"
+      this.resFlag =true
+    }
+    else{
+      this.msg = "check your inputs"
+    }
   }
 
 }
