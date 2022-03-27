27fc4eb83acfa385e0008ad38cbfee408b7de148
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-27 11:23:47 +0200

    s16

--- session16/src/app/pages/home/home.component.ts
+++ session16/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  handleForm(myForm:NgForm){
+    console.log(myForm.value)
   }
 
 }
