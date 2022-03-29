1f1003a2f6e8cd68c3266442dd1658c5e681075a
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-29 13:32:43 +0200

    s18

--- session18/src/app/components/pages/user/register/register.component.ts
+++ session18/src/app/components/pages/user/register/register.component.ts
@@ -8,18 +8,40 @@ import { UserService } from 'src/app/providers/services/user.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  userData = {
+    name:"marwa",
+    age:null,
+    phone:"",
+    email:"",
+    gender:null,
+    password:""
+  }
+  phoneSpcialError = ""
   constructor(private _user:UserService) { }
 
   ngOnInit(): void {
   }
+
   rigsterUser(register:NgForm){
     // console.log(register.value)
-    if(register.valid){
-      this._user.registerUser(register.value).subscribe(
-        data=> console.log(data)
-      )
+    let pStatus = this.checkPhone(this.userData.phone)
+    if(register.valid && pStatus){
+      
+      console.log(register.value)
+      // this._user.registerUser(register.value).subscribe(
+      //   data=> console.log(data)
+      // )
     }
   }
-
+checkPhone(pNum:string):boolean{
+  if((pNum.startsWith('010')||pNum.startsWith('012')||pNum.startsWith('011')||pNum.startsWith('015'))&& pNum.length==11) {
+    this.phoneSpcialError=""
+    return true
+  }
+  else{
+    if(pNum.length!=11) this.phoneSpcialError= "invalid phone must be 11 numbers"
+    else  this.phoneSpcialError= "invalid phone format"
+    return false
+  }
+}
 }
